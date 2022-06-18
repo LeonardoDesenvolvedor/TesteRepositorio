04e60d9bbdca546cca57c2e37df997fae2016c71
Leonardo <leonardoaugusto43@hotmail.comm>
2022-06-18 12:13:43 -0300

    Initial

---
+++ ToDo/ToDo/Models/Tarefa.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDo.Models
+{
+    public class Tarefa
+    {
+        [Key]
+       public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }  
+        
+        public DateTime EndData { get; set; }
+
+        public bool Status { get; set; }
+
+    }
+}
